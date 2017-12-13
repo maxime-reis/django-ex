7e7afb0d35610bea0ef92e4f4d866e64c618717d
Maxime REIS <maxime.reis@cern.ch>
2017-12-13 15:25:17 +0100

    [WIP] Deploy hardwarelabs website.

--- project/urls.py
+++ project/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 from welcome.views import index, health
+from benchmarks.views import home, ssd_benchmarks, cpu_benchmarks
 
 urlpatterns = [
     # Examples:
@@ -12,7 +13,10 @@ urlpatterns = [
     url(r'^$', index),
     url(r'^health$', health),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^benchmarks/', include('benchmarks.urls')),
+    url(r'^ssd$', ssd_benchmarks),
+    url(r'^cpu$', cpu_benchmarks),
+    url(r'^home$', home),
+#    url(r'^benchmarks/', include('benchmarks.urls')),
 ]
 
 if settings.DEBUG:
