fc96524751573814c2bbc14bd8510e473c66de73
maxime-reis <maxime.reis@cern.ch>
2018-04-12 15:19:34 +0200

    Fix remote users middleware import.

--- project/settings.py
+++ project/settings.py
@@ -57,7 +57,7 @@ MIDDLEWARE = [
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'debug_toolbar.middleware.DebugToolbarMiddleware',
     # Remote user auth
-    'hardwarelabs_core.middleware.remote_user_attributes_middleware.RemoteUserAttrMiddleware',
+    'middleware.remote_user_attributes_middleware.RemoteUserAttrMiddleware',
 ]
 
 ROOT_URLCONF = 'project.urls'
